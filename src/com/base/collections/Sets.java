e2bafec1379d344a40e542b6f9afc507c8d1605b
git-tushar <tuhsarpatil8691@gmail.com>
2020-05-23 18:39:55 +0530

    sets updated!
    Signed-off-by: git-tushar <tuhsarpatil8691@gmail.com>

--- src/com/base/collections/Sets.java
+++ src/com/base/collections/Sets.java
@@ -5,13 +5,19 @@ import java.util.*;
 public class Sets {
     public static void myHashSet() {
         Set<String> set = new HashSet<>();
+        set.add("b1");
+        System.out.println(set);
     }
 
     public static void myTreeSet() {
         Set<String> tree = new TreeSet<>();
+        tree.add("b1");
+        System.out.println(tree);
     }
 
     public static void myLinkedSet() {
         Set<String> linkedSet = new LinkedHashSet<>();
+        linkedSet.add("b1");
+        System.out.println(linkedSet);
     }
 }
